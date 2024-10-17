a8fde4285885d0b09f8409e9a6a82ae2482783b8
Lukarry <146958674+Lukarry@users.noreply.github.com>
2024-10-17 17:51:31 +0800

    Bintree.c
    二叉树的前序，中序，后序遍历

---
+++ Bintree.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+//将指向结构体的指针命名为Bintree
+typedef struct Tree *Bintree;
+typedef int Element;
+struct Tree{
+    Element val;
+    Bintree left;
+    Bintree right;
+};
+Bintree createBintree(){
+    Bintree bt=NULL;
+    char ch;
+    scanf("%c",&ch);
+    if(ch!='#'){
+        bt=(Bintree)malloc(sizeof(struct Tree));
+        bt->val=ch;
+        bt->left=createBintree();
+        bt->right=createBintree();
+    }
+    return bt;
+}
+void Preorder(Bintree BT){
+    if(BT){
+        printf("%c",BT->val);
+        Preorder(BT->left);
+        Preorder(BT->right);
+    }
+
+}
+void Inorder(Bintree BT){
+    if(BT){
+        Inorder(BT->left);
+        printf("%c",BT->val);
+        Inorder(BT->right);
+    }
+
+}
+void postorder(Bintree BT){
+    if(BT){
+        postorder(BT->left);
+        postorder(BT->right);
+        printf("%c",BT->val);
+
+    }
+}
+int main() {
+    Bintree tree = createBintree();
+    printf("Pre-order:");
+    Preorder(tree);
+    printf("\n");
+    printf("in-order:");
+    Inorder(tree);
+    printf("\n");
+    printf("post-order:");
+    postorder(tree);
+}
